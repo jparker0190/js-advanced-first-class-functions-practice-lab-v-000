565cf1e40a99b3498ecb6f63eb9b092fdffa3904
Joseph Parker <jparker010190@gmail.com>
2018-07-08 20:47:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
-const logDriverNames = function logDriverNames (drive){
+function logDriverNames (drive){
   drive.forEach(function(product){console.log(${product.name})})
 }
