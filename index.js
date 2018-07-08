3a103934782ad6545751cef23007eb928caadd1f
Joseph Parker <jparker010190@gmail.com>
2018-07-08 20:54:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
-const logDriverNames = function(driver, el, arr){
+function logDriverNames (driver, el, arr){
   console.log(${driver.name})
   return [...driver, el.name]
 }
