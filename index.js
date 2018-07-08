cf67b290c30644220f32d607b5f69ba9773b653e
Joseph Parker <jparker010190@gmail.com>
2018-07-08 21:53:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logDriverNames = function (driver){
 }
 const logDriversByHometown = function(driver, location){
   var arry = driver.map(function(num){
-    return num.hometown === driver.location
+    return num.hometown === driver.hometown
   })
   console.log(arry)
 }
