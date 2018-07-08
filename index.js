aed382f16390a33ae7b6a1c0aa6d6c0e8fe6feec
Joseph Parker <jparker010190@gmail.com>
2018-07-08 21:33:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logDriverNames = function (driver){
 }
 const logDriversByHometown = function(driver, location){
   var arry = driver.filter(function(num){
-    console.log(num)
+    console.log(num.location)
     return num.hometown === num.location
   })
   var art = arry.forEach(function(element){console.log(element.hometown)})
